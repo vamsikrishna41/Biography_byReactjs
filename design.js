0cb5ad452883d6c23b2fad037e6139698594b4a6
Bathula Vamsi Krishna <121428816+vamsikrishna41@users.noreply.github.com>
2023-07-28 15:37:55 +0530

    Add files via upload

---
+++ design.js
@@ -0,0 +1,32 @@
+ReactDOM.render(
+    <div>
+        <h1>Alessandro Volta</h1>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/5/52/Alessandro_Volta.jpeg"></img>
+        <h3>Born : 18 February 1745 Como, Duchy of Milan</h3>
+        <h3>Died : 5 March 1827 (aged 82) Como, Lombardy-Venetia, Austrian Empire</h3>
+        <h3>KNOWN FOR INVENTION OF</h3>	
+        <ol>
+            <li>The electric cell</li>
+            <li>Discovery of methane</li>
+            <li>Volt</li>
+            <li>Voltage</li>
+            <li>Voltmeter</li>
+        </ol>
+        <h3>AWARDS</h3>
+        <ol>
+            <li>Copley Medal (1794)</li>
+            <li>Legion of Honour</li>
+            <li>Order of the Iron Crown</li>
+        </ol>
+        <p>Alessandro Giuseppe Antonio Anastasio Volta was an Italian physicist and chemist who was a pioneer of electricity and power who is credited as the inventor of the electric battery and the discoverer of methane. He invented the voltaic pile in 1799, and reported the results of his experiments in 1800. With this invention Volta proved that electricity could be generated chemically and debunked the prevalent theory that electricity was generated solely by living beings. Volta's invention sparked a great amount of scientific excitement and led others to conduct similar experiments, which eventually led to the development of the field of electrochemistry.</p>
+        <h2>Early Life and Work</h2>
+        <p>Volta was born in Como, a town in northern Italy, on 18 February 1745. In 1794, Volta married an aristocratic lady also from Como, Teresa Peregrini, with whom he raised three sons: Zanino, Flaminio, and Luigi. His father, Filippo Volta, was of noble lineage. His mother, Donna Maddalena, came from the family of the Inzaghis.
+In 1774, he became a professor of physics at the Royal School in Como. A year later, he improved and popularised the electrophorus, a device that produced static electricity. His promotion of it was so extensive that he is often credited with its invention, even though a machine operating on the same principle was described in 1762 by the Swedish experimenter Johan Wilcke. In 1777, he travelled through Switzerland, where he befriended H. B. de Saussure.
+In the years between 1776 and 1778, Volta studied the chemistry of gases. He researched and discovered methane after reading a paper by Benjamin Franklin of the United States on "flammable air". In November 1776, he found methane in the marshes of Angera on Lake Maggiore, and by 1778 he managed to isolate methane.He devised experiments such as the ignition of methane by an electric spark in a closed vessel.
+Volta also studied what we now call electrical capacitance, developing separate means to study both electrical potential difference (V) and charge (Q), and discovering that for a given object, they are proportional. This is called Volta's Law of Capacitance, and for this work the unit of electrical potential has been named the volt.
+In 1779, he became a professor of experimental physics at the University of Pavia, a chair that he occupied for almost 40 years. Volta's lectures were so crowded with students that the subsequent emperor Joseph II ordered the construction (based on a project by Leopold Pollack) of a new "physical theater", today the "Aula Volta". Furthermore, the emperor granted Volta substantial funding to equip the physics cabinet with instruments, purchased by Volta in England and France. At the University History Museum of the University of Pavia there are 150 of them, used by Alessandro Volta.</p>
+    <h2>Last years and retirement</h2>
+    <p>In 1809, Volta became associated member of the Royal Institute of the Netherlands. In honour of his work, Volta was made a count by Napoleon Bonaparte in 1810.
+Volta retired in 1819 to his estate in Camnago, a frazione of Como, Italy, now named "Camnago Volta" in his honour. He died there on 5 March 1827, just after his 82nd birthday.Volta's remains were buried in Camnago Volta.</p>
+    </div>,document.getElementById("biography")
+);
